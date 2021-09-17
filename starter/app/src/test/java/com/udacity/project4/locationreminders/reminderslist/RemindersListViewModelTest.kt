4ba8f7e03047d14cb32b475321622c5a469ae176
thazin <nwe.nwethazin@gmail.com>
2021-09-17 23:22:31 +0800

    chore (Fake Data Source) : add test

--- starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ starter/app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,86 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.After
+import org.koin.core.context.stopKoin
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
 class RemindersListViewModelTest {
 
-    //TODO: provide testing to the RemindersListViewModel and its live data objects
+    private lateinit var viewModel: RemindersListViewModel
+    private lateinit var dataSource: FakeDataSource
 
+    private val reminder1 = ReminderDTO(
+        "Reminder1",
+        "Description1",
+        "Location1",
+        1.37,
+        103.84
+    )
+    private val reminder2 = ReminderDTO(
+        "Reminder2",
+        "Description2",
+        "Location2",
+        103.84,
+        1.37
+    )
+    private val remindersList = mutableListOf(reminder1, reminder2)
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Before
+    fun setupViewModel() {
+        dataSource = FakeDataSource(remindersList)
+        viewModel = RemindersListViewModel(ApplicationProvider.getApplicationContext(), dataSource)
+    }
+
+    @Test
+    fun loadReminders_loading() {
+        remindersList.add(reminder1)
+        remindersList.add(reminder2)
+
+        mainCoroutineRule.pauseDispatcher()
+
+        viewModel.loadReminders()
+        assertThat(viewModel.showLoading.getOrAwaitValue(), `is`(true))
+
+        mainCoroutineRule.resumeDispatcher()
+        assertThat(viewModel.showLoading.getOrAwaitValue(), `is`(false))
+        assertThat(viewModel.showNoData.getOrAwaitValue(), `is`(false))
+    }
+
+    @Test
+    fun noReminders_showEmpty() = runBlockingTest {
+        dataSource.deleteAllReminders()
+        viewModel.loadReminders()
+        assertThat(viewModel.showNoData.getOrAwaitValue(), `is`(true))
+    }
+
+    @After
+    fun tearDown() {
+        stopKoin()
+    }
 }
